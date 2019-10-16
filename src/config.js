4d7589f9a02a34ebf8b564ab932e50278e806b86
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-16 02:30:59 -0300

    Configurando o AWS Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-notes"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://fhcb9geia0.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_rltn8AczT",
+      APP_CLIENT_ID: "1d3un4pbu6i6gul04riei3l8au",
+      IDENTITY_POOL_ID: "us-east-1:21093e18-b716-4003-a39d-45e6e9797f4c"
+    }
+  };
