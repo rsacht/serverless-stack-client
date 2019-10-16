eb3e1687f2f9687e6bd0f70b9043ba4b1d48ff8a
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-16 02:07:39 -0300

    Agora, se você virar para o navegador e clicar no link de login, deverá ver o link destacado na barra de navegação. Além disso, ele não atualiza a página durante o redirecionamento

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 
 function App(props) {
@@ -16,8 +17,12 @@ function App(props) {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem href="/signup">Signup</NavItem>
-            <NavItem href="/login">Login</NavItem>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
