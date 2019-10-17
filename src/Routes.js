7adb749f3f7e53b0b2a80d164485cd5a94286683
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-16 22:36:12 -0300

    Incluindo o AppliedRoute nas rotaes que precisamos passar appProps

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,16 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import AppliedRoute from "./components/AppliedRoute";
 
 
-export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={Login} />
-      { /* Finally, catch all unmatched routes */ }
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+export default function Routes({ appProps }) {
+    return (
+      <Switch>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        { /* Finally, catch all unmatched routes */ }
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
