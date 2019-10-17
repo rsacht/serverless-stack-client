1e56f1bf57ff09d9e8aa278c2c540807d5fc4672
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-17 02:52:56 -0300

    Estamos definindo nossos campos usando a função que nosso Hook personalizado está retornando: onChange={handleFieldChange}

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
+import { Auth } from "aws-amplify";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
-import { Auth } from "aws-amplify";
+import { useFormFields } from "../libs/hooksLib";
 import "./Login.css";
 
 export default function Login(props) {
   const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     setIsLoading(true);
-  
+
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       props.userHasAuthenticated(true);
       props.history.push("/");
     } catch (e) {
@@ -36,16 +39,16 @@ export default function Login(props) {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>Password</ControlLabel>
           <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
             type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <LoaderButton
