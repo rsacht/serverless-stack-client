5a7fd1c709f890dd77bc738b3dcb83ba8a02ef80
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-17 03:12:32 -0300

    Fazendo o cadastro no AWS Cognito. Agora, se você mudar para o navegador e tentar se inscrever em uma nova conta, ele deverá redirecioná-lo para a página inicial depois que a inscrição for concluída com êxito.

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -7,6 +7,7 @@ import {
 } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import { useFormFields } from "../libs/hooksLib";
+import { Auth } from "aws-amplify";
 import "./Signup.css";
 
 export default function Signup(props) {
@@ -33,18 +34,37 @@ export default function Signup(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
+  
     setIsLoading(true);
-
-    setNewUser("test");
-
-    setIsLoading(false);
+  
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
-
+  
   async function handleConfirmationSubmit(event) {
     event.preventDefault();
-
+  
     setIsLoading(true);
+  
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+  
+      props.userHasAuthenticated(true);
+      props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
 
   function renderConfirmationForm() {
