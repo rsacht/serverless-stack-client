6a86fd9f19aa11c4b65d0998a023ea9e8e4e9f92
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-17 02:57:21 -0300

    Adicionando a rota da pagina de cadastro do usuário

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
-import AppliedRoute from "./components/AppliedRoute";
+
+
 
 
 export default function Routes({ appProps }) {
@@ -11,6 +14,7 @@ export default function Routes({ appProps }) {
       <Switch>
         <AppliedRoute path="/" exact component={Home} appProps={appProps} />
         <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
         { /* Finally, catch all unmatched routes */ }
         <Route component={NotFound} />
       </Switch>
