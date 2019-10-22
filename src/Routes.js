68069dd1bba0e323123662008e79bae27ec60d6c
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2019-10-22 01:05:54 -0300

    Criando a página de configurações

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 import NotFound from "./containers/NotFound";
 
 export default function Routes({ appProps }) {
@@ -16,6 +17,7 @@ export default function Routes({ appProps }) {
         <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
         <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
         <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+        <AppliedRoute path="/settings" exact component={Settings} appProps={appProps} />
         { /* Finally, catch all unmatched routes */ }
         <Route component={NotFound} />
       </Switch>
